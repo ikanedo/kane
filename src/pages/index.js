c8616c74ed64fbe88eb3167e14ba2b9681d9f80b
Kane Balagtey <kbalagtey@tacitknowledge.com>
2019-05-08 15:42:35 +0100

    test preview feature develop

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <p>Next line test.</p>
+    <p>Next line test.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
